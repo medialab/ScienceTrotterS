b0ca5d82a95dc556726f248bc21690249cee16c9
mydu <dumengying1989@gmail.com>
2020-10-02 16:18:53 +0200

    fix share

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -127,13 +127,13 @@ export class MyApp {
       if(role === 'close' && this.isAndroid) {
         this.showInstallBanner();
       }
-      if (role === 'close' && this.isIOS && ('share' in window.navigator) && window.navigator['share']) {
+      if (role === 'close' && this.isIOS && ('share' in navigator) && navigator['share']) {
         let shareData = {
           title: 'sts',
           text: 'sts',
           url: window.location.href,
         }
-        window.navigator.share(shareData)
+        navigator.share(shareData)
       }
     });
 
