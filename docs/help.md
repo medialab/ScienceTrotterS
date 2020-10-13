6c91c957c8ff40d6dbcc28c0fe5bd4cd83f84f2c
mydu <dumengying1989@gmail.com>
2020-10-13 11:59:34 +0200

    clean repo

--- docs/help.md
+++
@@ -1,4 +0,0 @@
-# Science Trotters APP
-
-* NODE 8.0.0
-* npm install @ionic/app-scripts@latest --save
--- prepare_apk_google_play.sh
+++
@@ -1,3 +0,0 @@
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore STS_release.keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk STS
-rm ./platforms/android/app/build/outputs/apk/release/STS_prod.apk
-zipalign -v 4  ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./platforms/android/app/build/outputs/apk/release/STS_prod.apk
